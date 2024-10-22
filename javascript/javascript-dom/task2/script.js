e431c7373caf9e91248f060f56848239f4d9cb10
SALMACODER <salmaghalzai95@gmail.com>
2024-10-22 21:41:07 +0500

    task3 completed

---
+++ javascript/javascript-dom/task2/script.js
@@ -0,0 +1,11 @@
+document.getElementById('text').style.display = 'none';
+ 
+//  var txt = document.getElementById('text');
+//  function button(){
+//     //
+//     // document.getElementById('text').style.display = 'block';
+//     var btn;
+//     if(txt.idName == 'text'){
+//       btn.style.display = 'none';
+//     }
+//  }
